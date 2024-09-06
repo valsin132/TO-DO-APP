fad03f6f077d9716ffab1e828ce6a600c85460d6
Valdemaras Sinkevicius <valdemarassinkevicius@gmail.com>
2024-09-06 12:20:12 +0300

    added complited icon and functionality

--- src/components/TaskDetails.jsx
+++ src/components/TaskDetails.jsx
@@ -1,21 +1,30 @@
 /* eslint-disable react/prop-types */
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 // import { ThemeContext } from '../contexts/ThemeContext';
 import { ToDoContext } from '../contexts/ToDoContext';
 import { RiDeleteBin2Line } from "react-icons/ri";
+import { MdOutlineDoneOutline } from "react-icons/md";
 
 const TaskDetails = ({task}) => {
+    const [completed, setCompleted] = useState(false);
     const {removeTask} = useContext(ToDoContext);
     // const {isLightTheme, light, dark} = useContext(ThemeContext);
     // const theme = isLightTheme ? light : dark;
 
+    const handleComplete = () => {
+        setCompleted(!completed)
+    }
+
     return ( 
-        <li>
+        <li className={completed ? 'task-completed' : ''}>
             <div>
                 <div className="task">{task.task}</div>
                 <div className="description">{task.description}</div>
             </div>
-            <RiDeleteBin2Line className='delete-icon' onClick={() => removeTask(task.id)} />
+            <div>
+                <RiDeleteBin2Line className='delete-icon' onClick={() => removeTask(task.id)} />
+                <MdOutlineDoneOutline className='complete-icon' onClick={handleComplete} />
+            </div>
         </li>
      );
 }
