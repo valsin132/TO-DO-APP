a3e73f275d849f8b3108811002a5429322e0b6b9
Valdemaras Sinkevicius <valdemarassinkevicius@gmail.com>
2024-09-06 10:52:20 +0300

    changed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,12 @@ const Navbar = () => {
     const {tasks} = useContext(ToDoContext);
     
     return (
-        <nav>
-            <div className='navbar'>
-                <h1><FaTasks /> To-Do List</h1>
-                <p>All my tasks: {tasks.length}</p>
+        <nav className='nav-wrapper'>
+            <div className='logo'>
+                <FaTasks className='logo-icon' />
+                <h1>To-Do List</h1>
             </div>
+            <p>All my tasks: {tasks.length}</p>
         </nav>
     );
 }
