b9fb566995d78681e51686f1b0e0c4c3b42aff24
Valdemaras Sinkevicius <valdemarassinkevicius@gmail.com>
2024-09-10 10:05:50 +0300

    changed icons

--- src/components/TaskDetails.jsx
+++ src/components/TaskDetails.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
 import { useContext } from 'react';
 import { ToDoContext } from '../contexts/ToDoContext';
-import { RiDeleteBin2Line } from "react-icons/ri";
-import { MdOutlineDoneOutline } from "react-icons/md";
+import { IoTrashBinOutline } from "react-icons/io5";
+import { MdDone } from "react-icons/md";
 
 const TaskDetails = ({task}) => {
     const {removeTask, toggleTaskCompletion} = useContext(ToDoContext);
@@ -18,8 +18,8 @@ const TaskDetails = ({task}) => {
                 <div className='description'>{task.description}</div>
             </div>
             <div className='task-icons'>
-                <RiDeleteBin2Line className='delete-icon' onClick={() => removeTask(task.id)} />
-                <MdOutlineDoneOutline className='complete-icon' onClick={handleComplete} />
+                <IoTrashBinOutline className='delete-icon' onClick={() => removeTask(task.id)} />
+                <MdDone className='complete-icon' onClick={handleComplete} />
             </div>
         </li>
      );
