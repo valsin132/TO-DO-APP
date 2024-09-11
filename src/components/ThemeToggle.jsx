0c5e85426ee4f2f228e1d81c282f1d91e9d5e3fc
Valdemaras Sinkevicius <valdemarassinkevicius@gmail.com>
2024-09-11 17:55:01 +0300

    added theme toggle button

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,11 +1,16 @@
 import { useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
-import { MdLightMode } from "react-icons/md";
+// import { MdLightMode } from "react-icons/md";
 
 const ThemeToggle = () => {
     const {toggleTheme} = useContext(ThemeContext)
     return ( 
-        <MdLightMode className='theme-mode-icon' onClick={toggleTheme} />
+        // <MdLight className='theme-mode-icon' onClick={toggleTheme} />
+        // <button className='button' onClick={toggleTheme}></button>
+        <div className='theme-mode-icon'>
+        <input type="checkbox" name="switch" id="switch"/>
+        <label onClick={toggleTheme} htmlFor="switch"></label>
+        </div>
      );
 }
  
